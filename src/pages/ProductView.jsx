6ea5e27340718cd627f260ada47a8d2d8feeaa27
viswajith-7885 <viswajith21295@gmail.com>
2025-09-29 17:18:40 +0530

    lastcommit

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import {  useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { CartContext } from "../context/Cartcontext";
+import { CartContext } from "../context/Cartcontext.js";
 
 
 export default function ProductView() {
