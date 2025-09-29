212bfd94740b9c111a851bb0b563bc50c444c7b3
viswajith-7885 <viswajith21295@gmail.com>
2025-09-29 17:14:13 +0530

    fix-cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../context/Cartcontext";
 
 export default function Cart() {
   const { cartItems, removeFromCart, clearCart } = useContext(CartContext);
